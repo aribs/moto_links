69a6f1dd12d430361c7840d0224259b9c71914d3
aribs <alejoribes@gmail.com>
2016-02-02 21:52:29 +0100

    Ad view

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -1,19 +1,22 @@
 class LinkController < ApplicationController
-  before_filter :restrict_access
-  respond_to :json
+
 
   def index
     @links = Link.all
+    @link = Link.new
   end
   def new
-    @link = Link.new(link_params)
+    @link = Link.new
+  end
+  def show
+    @link = Link.find(link_params)
   end
   def create
     @link = Link.new(link_params)
     respond_to do |format|
       if @link.save
         format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: link }
+        format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new }
         format.json { render json: link.errors, status: :unprocessable_entity }
@@ -29,10 +32,7 @@ class LinkController < ApplicationController
   end
   private
   def link_params
-    params.require(:link).permit(:url, :quality, :type, :reports, :language, :token)
+    params.require(:link).permit(:url, :quality, :type, :reports, :language)
   end
-  def restrict_access
-    api_key = ApiKey.find_by_access_token(params[:access_token])
-    head :unauthorized unless api_key
-  end
+
 end
